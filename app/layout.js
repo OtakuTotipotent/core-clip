44a87ec440a30bdc2cd4121df72e77af59b39ba5
Afnan Muhammad <otakutotipotent@gmail.com>
2026-02-12 11:54:02 +0500

    chore: add sans & mono fonts

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,15 @@
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Outfit } from "next/font/google"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Core-Clip | Homepage",
@@ -10,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className="{Outfit}"
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
