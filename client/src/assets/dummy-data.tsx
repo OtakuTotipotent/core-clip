6f8279fb0b8a63cd87f86239be7f475fca17f659
Afnan Muhammad <otakutotipotent@gmail.com>
2026-03-04 08:29:55 +0500

    feat: add footer

---
+++ client/src/assets/dummy-data.tsx
@@ -0,0 +1,29 @@
+export const footerLinks = [
+  {
+    title: "title",
+    links: [
+      { name: "link", url: "#" },
+      { name: "link", url: "#" },
+      { name: "link", url: "#" },
+      { name: "link", url: "#" },
+    ],
+  },
+  {
+    title: "title",
+    links: [
+      { name: "link", url: "#" },
+      { name: "link", url: "#" },
+      { name: "link", url: "#" },
+      { name: "link", url: "#" },
+    ],
+  },
+  {
+    title: "title",
+    links: [
+      { name: "link", url: "#" },
+      { name: "link", url: "#" },
+      { name: "link", url: "#" },
+      { name: "link", url: "#" },
+    ],
+  },
+];
