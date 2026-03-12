eb91aa7220df99f3d1388f20ac1a6d8b3e85b7c5
OtakuTotipotent <otakutotipotent@gmail.com>
2026-03-12 14:23:01 +0500

    init: add pages templates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,23 @@
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import SoftBackdrop from './components/SoftBackdrop';
-import Footer from './components/Footer';
-import LenisScroll from './components/lenis';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import SoftBackdrop from "./components/SoftBackdrop";
+import Footer from "./components/Footer";
+import LenisScroll from "./components/lenis";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-	return (
-		<>
-			<SoftBackdrop />
-			<LenisScroll />
-			<Navbar />
-			<Home />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <SoftBackdrop />
+      <LenisScroll />
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
 }
 export default App;
