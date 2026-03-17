df3db1fd63fe0c7f6a870a13fa3cfb3161878920
OtakuTotipotent <otakutotipotent@gmail.com>
2026-03-17 09:59:34 +0500

    feat: add server base template and packages

---
+++ server/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// paths
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
+});
+
+// server initialization
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
