75b1442916ea0de5b513c3d2fa4b8a6e2ff453ab
OtakuTotipotent <otakutotipotent@gmail.com>
2026-03-18 12:44:03 +0500

    feat: add clerk auth

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,23 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerk.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
+
+app.post(
+  "/api/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhooks,
+);
+
 app.use(express.json());
+app.use(clerkMiddleware());
 
 // paths
 app.get("/", (req: Request, res: Response) => {
