1cf329a6379960c6cde2b0cbce7b0d4376f7398c
OtakuTotipotent <otakutotipotent@gmail.com>
2026-05-06 10:57:14 +0500

    feat: add cors & json to server

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,19 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
 
 const app = express();
 
-const port = 3000;
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
